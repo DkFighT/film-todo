41e1820c053b7dea0b8e0888322193ba4d6df20a
DkFighT <116903571+DkFighT@users.noreply.github.com>
2023-03-21 01:39:28 +0300

    Update script.js

--- animeTODO/js/script.js
+++ animeTODO/js/script.js
@@ -76,9 +76,3 @@ function handleTouchMove(event){
     x = null;
     y = null;
 }
-
-// Изменение высоты
-window.addEventListener('resize', (event) => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--allscreen', `${vh}px`);
-  });
