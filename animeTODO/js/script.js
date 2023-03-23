cfb4ba795b06dafcd78a40962d64408fa20afabc
DkFighT <116903571+DkFighT@users.noreply.github.com>
2023-03-23 14:45:05 +0300

    Add files via upload

--- animeTODO/js/script.js
+++ animeTODO/js/script.js
@@ -100,3 +100,39 @@ window.addEventListener('resize', () => {
     document.documentElement.style.cssText = `--allscreen: ${window.innerHeight}px`;
     setTimeout(window.scrollTo(0, 1), 10);
 })
+
+// мобильный фокус поиск
+let mobile_search = document.querySelector('.search-bottom');
+
+mobile_search.onclick = () => {
+    let mobile_input = document.getElementById('mobile-search-form');
+    mobile_search.classList.add('active-search');
+    mobile_input.style.width = '35vmin';
+    mobile_input.addEventListener('focusout', () => {
+        mobile_input.style.width = '0';
+        mobile_input.removeAttribute('autofocus');
+        mobile_search.classList.remove('active-search');
+    })
+}
+
+// desktop поиск
+let search = document.getElementById("search-field");
+search.oninput = function () {
+    let lists = document.querySelectorAll("#point-name");
+    let search_val = document.getElementById("search-field");
+    if (search_val.value != '') {
+        lists.forEach(list => {
+            if (list.value.search(search_val.value) != -1) {
+                list.parentElement.parentElement.style.display = "flex";
+            }
+            else {
+                list.parentElement.parentElement.style.display = "none";
+            }
+        });
+    }
+    else {
+        lists.forEach(list => {
+            list.parentElement.parentElement.style.display = "flex";
+        })
+    }
+}
